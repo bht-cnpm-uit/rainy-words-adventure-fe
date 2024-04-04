fce7e83ee693340d8e798f871fc7ace9aab8d8e2
lehuudoUIT <22520254@gm.uit.edu.vn>
2024-04-04 16:30:08 +0700

    UI without animation

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,8 @@ import VocabLib from '../pages/VocabLib';
 import DifficultyComponent from '../pages/Game/DifficultyComponent';
 import Level from '../pages/Game/Level';
 import WordCollect from '../pages/Game/WordCollect';
-import LoginForm from '../pages/Login/login';
+//import LoginForm from '../pages/Login/login';
+import LoginForm from '../pages/Login/loginReal';
 // Public routes
 const publicRoutes = [
     {
