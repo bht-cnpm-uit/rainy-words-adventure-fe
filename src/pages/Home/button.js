35bc34c542b4b9e6b0d5df3670bc3412c3b83e5d
22520743@gm.uit.edu.vn <22520743@gm.uit.edu.vn>
2024-04-08 21:33:05 +0700

    fix background

--- src/pages/Home/button.js
+++ src/pages/Home/button.js
@@ -8,8 +8,7 @@ class Button {
 
     }
     draw(context) {
-        context.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.spriteWidth / 2.5, this.spriteHeight / 2.5);
-
+        context.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.spriteWidth / 2, this.spriteHeight / 2);
     }
     onClick(context) {
 
@@ -26,15 +25,15 @@ export class Guide extends Button {
         this.spriteHeight = 279;
         this.x = 30;
         this.y = -10;
-        this.translateX = this.game.width * 1 / 2;
-        this.translateY = this.game.height * 1 / 2;
+        // this.translateX = this.game.width * 1 / 2;
+        // this.translateY = this.game.height * 1 / 2;
     }
     draw(context) {
         super.draw(context);
         context.font = "30px Comic Sans MS";
         context.fillStyle = "brown";
         context.textAlign = "center";
-        context.fillText("Hướng dẫn", -this.x * 3.4 + this.spriteWidth / 2, this.y + this.spriteHeight / 2.5 - 20)
+        context.fillText("Hướng dẫn", -this.x * 2.7 + this.spriteWidth/2, this.y + this.spriteHeight/2.5)
     }
 }
 
@@ -45,19 +44,16 @@ export class start extends Button {
         this.image.src = '../assets/Asset/ButtonAtlas_cuts/ButtonAtlas_cuts/image_25.png';
         this.spriteWidth = 437;
         this.spriteHeight = 129;
-        this.x = this.width / 2 + this.spriteWidth / 3 + 40;
-        this.y = this.height / 2 + this.spriteHeight * 1.7;
-        this.translateX = this.game.width * 1 / 3;
-        this.translateY = this.game.height * 1 / 3;
+        this.x = this.width /1.55 ;
+        this.y = this.height / 1.3 ;
     }
     draw(context) {
         super.draw(context);
         context.font = "bold 30px Arial";
         context.fillStyle = "brown";
         context.fontWeight = "bolder";
-
         context.textAlign = "center";
-        context.fillText("BẮT ĐẦU", this.width / 2 + this.spriteWidth / 3 + 127, this.height / 2 + this.spriteHeight * 1.5 + 60)
+        context.fillText("BẮT ĐẦU", this.x + this.spriteWidth/4, this.y + this.spriteHeight/3)
     }
 }
 
