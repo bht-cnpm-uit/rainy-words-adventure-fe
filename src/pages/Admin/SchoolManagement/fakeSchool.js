b1408958e672f2818d660f54bc7847a8715d459a
22520743@gm.uit.edu.vn <22520743@gm.uit.edu.vn>
2024-04-22 21:14:07 +0700

    student and school admin UI

---
+++ src/pages/Admin/SchoolManagement/fakeSchool.js
@@ -0,0 +1,203 @@
+export const fakeData = [
+    {
+        ID: 1,
+        name: 'New School Name 1',
+    },
+    {
+        ID: 2,
+        name: 'New School Name 2',
+    },
+    {
+        ID: 3,
+        name: 'New School Name 3',
+    },
+    {
+        ID: 4,
+        name: 'New School Name 4',
+    },
+    {
+        ID: 5,
+        name: 'New School Name 5',
+    },
+    {
+        ID: 6,
+        name: 'New School Name 6',
+    },
+    {
+        ID: 7,
+        name: 'New School Name 6',
+    },
+
+    {
+        ID: 8,
+        name: 'New School Name 8',
+    },
+    {
+        ID: 9,
+        name: 'New School Name 9',
+    },
+    {
+        ID: 10,
+        name: 'New School Name 10',
+    },
+    {
+        ID: 11,
+        name: 'New School Name 11',
+    },
+    {
+        ID: 12,
+        name: 'New School Name 12',
+    },
+    {
+        ID: 13,
+        name: 'New School Name 13',
+    },
+    {
+        ID: 14,
+        name: 'New School Name 14',
+    },
+    {
+        ID: 15,
+        name: 'New School Name 15',
+    },
+    {
+        ID: 16,
+        name: 'New School Name 16',
+    },
+    {
+        ID: 17,
+        name: 'New School Name 17',
+    },
+    {
+        ID: 18,
+        name: 'New School Name 18',
+    },
+    {
+        ID: 19,
+        name: 'New School Name 19',
+    },
+    {
+        ID: 20,
+        name: 'New School Name 20',
+    },
+    {
+        ID: 21,
+        name: 'New School Name 21',
+    },
+    {
+        ID: 22,
+        name: 'New School Name 22',
+    },
+    {
+        ID: 23,
+        name: 'New School Name 23',
+    },
+    {
+        ID: 24,
+        name: 'New School Name 24',
+    },
+    {
+        ID: 25,
+        name: 'New School Name 25',
+    },
+    {
+        ID: 26,
+        name: 'New School Name 26',
+    },
+    {
+        ID: 27,
+        name: 'New School Name 27',
+    },
+    {
+        ID: 28,
+        name: 'New School Name 28',
+    },
+    {
+        ID: 29,
+        name: 'New School Name 29',
+    },
+    {
+        ID: 30,
+        name: 'New School Name 30',
+    },
+    {
+        ID: 31,
+        name: 'New School Name 31',
+    },
+    {
+        ID: 32,
+        name: 'New School Name 32',
+    },
+    {
+        ID: 33,
+        name: 'New School Name 33',
+    },
+    {
+        ID: 34,
+        name: 'New School Name 34',
+    },
+    {
+        ID: 35,
+        name: 'New School Name 35',
+    },
+    {
+        ID: 36,
+        name: 'New School Name 36',
+    },
+    {
+        ID: 37,
+        name: 'New School Name 37',
+    },
+    {
+        ID: 38,
+        name: 'New School Name 38',
+    },
+    {
+        ID: 39,
+        name: 'New School Name 39',
+    },
+    {
+        ID: 40,
+        name: 'New School Name 40',
+    },
+    {
+        ID: 41,
+        name: 'New School Name 41',
+    },
+    {
+        ID: 42,
+        name: 'New School Name 42',
+    },
+    {
+        ID: 43,
+        name: 'New School Name 43',
+    },
+    {
+        ID: 44,
+        name: 'New School Name 44',
+    },
+    {
+        ID: 45,
+        name: 'New School Name 45',
+    },
+    {
+        ID: 46,
+        name: 'New School Name 46',
+    },
+    {
+        ID: 47,
+        name: 'New School Name 47',
+    },
+    {
+        ID: 48,
+        name: 'New School Name 48',
+    },
+    {
+        ID: 49,
+        name: 'New School Name 49',
+    },
+    {
+        ID: 50,
+        name: 'New School Name 50',
+    },
+];
--- src/pages/Admin/SchoolManagement/index.jsx
+++ src/pages/Admin/SchoolManagement/index.jsx
@@ -1,9 +1,137 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DataTable from 'react-data-table-component';
+import { fakeData } from './fakeSchool';
 
 const SchoolManagement = () => {
+    const [editID, setEditID] = useState(null);
+    const [deleteID, setDeleteID] = useState(null);
+    const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+
+    const handleEdit = (id) => {
+        setEditID(id);
+    };
+
+    const handleSaveEdit = (id) => {
+        // Xử lý logic lưu sửa đổi dữ liệu với ID là id
+        setEditID(null);
+    };
+
+    const handleCancelEdit = () => {
+        setEditID(null);
+    };
+
+    const handleDelete = (id) => {
+        setDeleteID(id);
+        setShowConfirmationModal(true); // Hiển thị popup xác nhận trước khi xóa
+    };
+
+    const handleConfirmDelete = () => {
+        // Xử lý logic xóa dữ liệu với ID là deleteID
+        setDeleteID(null);
+        setShowConfirmationModal(false); // Ẩn popup xác nhận sau khi xác nhận xóa
+    };
+
+    const handleCancelDelete = () => {
+        setDeleteID(null);
+        setShowConfirmationModal(false); // Ẩn popup xác nhận nếu người dùng hủy bỏ
+    };
+
+    const [isOpenModelImportData, setIsOpenModelImportData] = useState(false);
+
+    const columns = [
+        {
+            name: 'ID',
+            selector: 'ID',
+            sortable: true,
+            width: '10%',
+        },
+        {
+            name: 'Tên trường',
+            selector: 'name',
+            sortable: true,
+            width: '60%',
+        },
+        {
+            name: 'Thao tác',
+            width: '30%',
+            cell: (row) => {
+                return (
+                    <div>
+                        <button className="h-6 w-6" onClick={() => {}} data-tag="allowRowEvents">
+                            <svg
+                                className="fill-current text-zinc-500 hover:text-red-500"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 448 512"
+                            >
+                                <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+                            </svg>
+                        </button>
+                        <button
+                            className="ml-5 h-6 w-6"
+                            onClick={() => {}}
+                            data-tag="allowRowEvents"
+                        >
+                            <svg
+                                className="fill-current text-zinc-500 hover:text-blue-500"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                            >
+                                <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
+                            </svg>
+                        </button>
+                    </div>
+                );
+            },
+        },
+    ];
+
     return (
-        <div className="h-screen inset-0 bg-black text-white flex items-center justify-center">
-            <span>School Management Component</span>
+        <div className="container">
+            <DataTable
+                title="Danh sách trường học"
+                columns={columns}
+                data={fakeData}
+                pagination
+                customStyles={{
+                    headRow: {
+                        style: {
+                            backgroundColor: '#f0f0f0',
+                        },
+                    },
+                    headCells: {
+                        style: {
+                            fontWeight: 'bold',
+                            color: '#333',
+                        },
+                    },
+                }}
+                paginationComponentOptions={{
+                    rowsPerPageText: 'Filas por página',
+                    rangeSeparatorText: 'de',
+                    selectAllRowsItem: true,
+                    selectAllRowsItemText: 'Todos',
+                }}
+                fixedHeader
+                fixedHeaderScrollHeight="570px"
+                subHeader
+                subHeaderComponent={
+                    <div className="sub">
+                        <button
+                            className="left-0 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                            onClick={() => setIsOpenModelImportData(!isOpenModelImportData)}
+                        >
+                            <p>Thêm trường học</p>
+                        </button>
+
+                        <button
+                            className="ml-4 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                            onClick={() => setIsOpenModelImportData(!isOpenModelImportData)}
+                        >
+                            <p>Nhập dữ liệu</p>
+                        </button>
+                    </div>
+                }
+            />
         </div>
     );
 };
