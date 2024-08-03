27c226a0a7941302e47789b8adeb35aed9363f5d
bmhungqb <21522110@gm.uit.edu.vn>
2024-08-03 16:26:12 +0700

    update

--- src/services/studentServices.js
+++ src/services/studentServices.js
@@ -20,31 +20,16 @@ const updateAvatar = async (data) => {
 
 const getStudentInfo = (id) => {
     return instance.get(`api/user/get-info/${id}`);
-<<<<<<< HEAD
 }
-=======
-};
-
->>>>>>> 2a75200f0b684de8aa1c7d42f0c3cac274b8f5f5
 const handleLogin = (phoneNumber, password) => {
     return instance.post('api/login', { phoneNumber: phoneNumber, password: password });
 };
 const handleSignUp = (dataSignUp) => {
-<<<<<<< HEAD
     return instance.post('api/user/signup', dataSignUp)
 }
 const getListWordsOfStudent = (id) => {
     return instance.post('api/student-word/get-all', id);
 }
-
-export { getAllStudents, getItemsOfStudent, getAchivementOfStudent, updateInfo, getStudentInfo, handleLogin, handleSignUp, updateAvatar, getListWordsOfStudent }                  
-=======
-    return instance.post('api/user/signup', dataSignUp);
-};
-
-const getListWordsOfStudent = (id) => {
-    return instance.post('api/student-word/get-all', id);
-};
 export {
     getAllStudents,
     getItemsOfStudent,
@@ -56,4 +41,3 @@ export {
     updateAvatar,
     getListWordsOfStudent
 };
->>>>>>> 2a75200f0b684de8aa1c7d42f0c3cac274b8f5f5
