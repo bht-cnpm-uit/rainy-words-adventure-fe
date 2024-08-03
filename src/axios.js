e00305ec2de7fdda5c8975f6d23783f89baf4791
bmhungqb <21522110@gm.uit.edu.vn>
2024-08-03 23:44:19 +0700

    .

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: process.env.REACT_APP_BACKEND_URL,
-    baseURL: "http://localhost:8080",
-    withCredentials: true
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+    // baseURL: "http://localhost:8080",
+    // withCredentials: true
 })
 instance.interceptors.response.use(
     (response) => {
