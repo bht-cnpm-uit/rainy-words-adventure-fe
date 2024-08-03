6f156b66b37cee658a58d2d3d8a4fcfd838a6dbb
Lam Pham <127223170+lampuit@users.noreply.github.com>
2024-08-03 14:31:45 +0700

    resolve conflict

--- src/services/studentServices.js
+++ src/services/studentServices.js
@@ -1,39 +1,45 @@
-import instance from "../axios";
+import instance from '../axios';
 const getAllStudents = () => {
-    return instance.get('api/user/get-all-student')
-}
+    return instance.get('api/user/get-all-student');
+};
 const getItemsOfStudent = (id) => {
     return instance.get(`api/item/get-student/${id}`);
-}
+};
 
 const getAchivementOfStudent = (id) => {
     return instance.get(`api/user/get-achievement/${id}`);
-}
+};
 
 const updateInfo = async (data) => {
     return instance.post('api/user/update-info', data);
-}
+};
 
 const updateAvatar = async (data) => {
-    return instance.post('api/user/update-avatar', data)
-}
+    return instance.post('api/user/update-avatar', data);
+};
 
 const getStudentInfo = (id) => {
     return instance.get(`api/user/get-info/${id}`);
-}
-<<<<<<< HEAD
+};
+
 const handleLogin = (phoneNumber, password) => {
-    return instance.post('api/login', { phoneNumber: phoneNumber, password: password })
-}
+    return instance.post('api/login', { phoneNumber: phoneNumber, password: password });
+};
 const handleSignUp = (dataSignUp) => {
-    return instance.post('api/user/signup', dataSignUp)
-}
-export { getAllStudents, getItemsOfStudent, getAchivementOfStudent, updateInfo, getStudentInfo, handleLogin, handleSignUp, updateAvatar }                  
-=======
+    return instance.post('api/user/signup', dataSignUp);
+};
 
-const getListWordsOfStudent = (id) => { 
-    return instance.post('api/student-word/get-all',id );
-}
-
-export { getAllStudents, getItemsOfStudent, getAchivementOfStudent, updateInfo, getStudentInfo, getListWordsOfStudent}                  
->>>>>>> 2c3bda5924037a0e0a6fe7fd1858ddfe30187a0e
+const getListWordsOfStudent = (id) => {
+    return instance.post('api/student-word/get-all', id);
+};
+export {
+    getAllStudents,
+    getItemsOfStudent,
+    getAchivementOfStudent,
+    updateInfo,
+    getStudentInfo,
+    handleLogin,
+    handleSignUp,
+    updateAvatar,
+    getListWordsOfStudent
+};
